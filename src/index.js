005d4131bcb9878ca30071b4f77a85619c905232
Thanos Dimitriades <78486373+thanosades@users.noreply.github.com>
2021-02-09 14:38:42 +0200

    exercise 1.5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
-  return (<h1>{props.course}</h1>)
+  return (<h1>{props.name}</h1>)
 }
 
 const Part = (props) => {
@@ -20,35 +20,39 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  const sum = props.parts.reduce((total, current) => { 
-    return total + current.exercises}, 0)
+  const sum = props.parts.reduce((total, current) => {
+    return total + current.exercises
+  }, 0)
 
   return (<p> Number of exercises {sum}
   </p>)
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
